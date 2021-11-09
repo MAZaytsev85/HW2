ea6e82725f0257228eccb274f79bc144abe5977a
MAZaytsev85 <streetrsr2007@yandex.ru>
2021-11-09 14:36:57 +0300

    Commit

---
+++ src/HWGeek5/StaffMain.java
@@ -0,0 +1,12 @@
+package HWGeek5;
+
+public class StaffMain {
+    public static void main(String[] args) {
+        Person Person1 = new Person("Boris", "Manager", "Boris@ya.ru","89223334455", 50000, 45);
+        Person1.ConsoleOut();
+        Person1.ArrayOut();
+
+
+    }
+}
+
