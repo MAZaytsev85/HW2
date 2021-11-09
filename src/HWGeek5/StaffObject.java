089bd5f681959e21a8e13199bc1df3f7db2b21d0
MAZaytsev85 <streetrsr2007@yandex.ru>
2021-11-09 21:21:45 +0300

    Commit

--- src/HWGeek5/StaffObject.java
+++ src/HWGeek5/StaffObject.java
@@ -1,38 +1,32 @@
 package HWGeek5;
 
-    class Person {
+class Person {
 
-        String name;
-        String position;
-        String mail;
-        String phone;
-        int cash;
-        int year;
-        Person[] persArray = new Person[5];
+    private String name;
+    private String position;
+    private String mail;
+    private String phone;
+    private int cash;
+    private int year;
 
 
-        void ConsoleOut() {
-            System.out.println(name + " " + position + " " + phone + " " + mail + " " + cash + " " + year);
-        }
+       public Person(String name, String position, String mail, String phone, int cash, int year) {
+           this.name = name;
+           this.position = position;
+           this.mail = mail;
+           this.phone = phone;
+           this.cash = cash;
+           this.year = year;
+       }
+           public void ConsoleOut() {
+               System.out.printf("Person: %s, pos: %s, mail: %s, phone: %s, cash: %d, year: %d\n", this.name, this.position, this.phone, this.mail, this.cash, this.year);
+       }
 
-        public Person(String name1, String position1, String mail1, String phone1, int cash1, int year1) {
-            name = name1;
-            position = position1;
-            mail = mail1;
-            phone = phone1;
-            cash = cash1;
-            year = year1;
-        }
+       public int getYear() {
+           return year;
+       }
 
-        void ArrayOut() {
-            persArray[0] = new Person("Boris", "Manager", "Boris@ya.ru", "89223334455", 50000, 45);
-            persArray[1] = new Person("Ivan", "Master", "Ivan@ya.ru", "89223334456", 30000, 43);
-            persArray[2] = new Person("Sergey", "Technical", "Sergey@ya.ru", "89223334457", 20000, 35);
-            persArray[3] = new Person("Evgeny", "Technical", "Evgeny@ya.ru", "89223334458", 20000, 33);
-            persArray[4] = new Person("Alexey", "Technical", "Alexey@ya.ru", "89223334459", 20000, 32);
-            for (int i = 0; i < persArray.length && year > 40; i++) {
-                System.out.println(persArray[i]);
-            }
-
-        }
-    }
+       public String toString() {
+           return name + position + phone + mail + cash + year;
+       }
+   }
